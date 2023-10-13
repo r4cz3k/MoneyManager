ec6dfac43f98144a21961e767cb7d4811182b2a0
Maciej Rak <raczek.maciek@gmail.com>
2023-10-13 10:25:56 +0200

    Initial Commit

---
+++ MoneyManager/MoneyManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoneyManagerApp.swift
+//  MoneyManager
+//
+//  Created by Maciej Rak on 13/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct MoneyManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
